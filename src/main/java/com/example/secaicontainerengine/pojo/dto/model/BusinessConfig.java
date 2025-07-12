60c1e0a159302c649c39fbf17f73472997a50126
CFZ <406047187@qq.com>
2025-07-12 18:27:21 +0800

    2025-7-12：修改BusinessConfig

--- src/main/java/com/example/secaicontainerengine/pojo/dto/model/BusinessConfig.java
+++ src/main/java/com/example/secaicontainerengine/pojo/dto/model/BusinessConfig.java
@@ -7,8 +7,19 @@ import java.util.List;
 @Data
 public class BusinessConfig {
 
+    // 每个维度的配置信息
+    private List<EvaluationDimensionConfig> evaluateMethods;
 
-    // 模型评测方法
-    private List<String> evaluateMethods;
+    @Data
+    public static class EvaluationDimensionConfig {
+        private String dimension; // 如 basic、robustness 等
+        private List<MethodMetricPair> methodMetricMap; // 测试方法和其对应的指标
+    }
+
+    @Data
+    public static class MethodMetricPair {
+        private String method;           // 测试方法名称
+        private List<String> metrics;    // 对应的指标
+    }
 
 }
